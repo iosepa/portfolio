70e70852d7e7c894d438ec286c194814d1d10a30
Joseph Oxborrow <j@climatejourney.com>
2019-05-15 22:03:40 -0500

    reword

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ window.onload = function () {
 
 
   //auto Type text
-  autoType("greeting", 95, "Hello. Are you looking for your next developer?");
+  autoType("greeting", 95, "Hello. Are you looking an angular developer?");
 
   //show button to contact form
   setTimeout(function () {
